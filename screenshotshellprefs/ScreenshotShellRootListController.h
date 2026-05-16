c8d4bbdaac0388ff94b07239fb23cf0de1417970
8946688-cloud <8946688@gmail.com>
2026-05-16 11:55:42 +0800

    Update ScreenshotShellRootListController.h

--- screenshotshellprefs/ScreenshotShellRootListController.h
+++ screenshotshellprefs/ScreenshotShellRootListController.h
@@ -1,5 +1,6 @@
 #import <Preferences/PSListController.h>
 #import <PhotosUI/PhotosUI.h>
+#import <UIKit/UIKit.h>
 
-@interface ScreenshotShellRootListController : PSListController <PHPickerViewControllerDelegate>
+@interface ScreenshotShellRootListController : PSListController <PHPickerViewControllerDelegate, UIDocumentPickerDelegate>
 @end
